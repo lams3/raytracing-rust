010bef7ba0a1023621d58d9973d32d8d1ab08e3e
Luca <lams3@cin.ufpe.br>
2021-02-02 20:43:02 -0300

    Space-time ray sampling

--- src/rendering/camera.rs
+++ src/rendering/camera.rs
@@ -1,5 +1,7 @@
 use crate::structures::{Point3, Vec3, Ray};
 
+use rand::{thread_rng, Rng};
+
 pub struct Camera {
     pub vertical_fov: f64,
     pub aspect_ratio: f64,
@@ -8,11 +10,13 @@ pub struct Camera {
     pub position: Point3,
     pub x_axis: Vec3,
     pub y_axis: Vec3,
-    pub z_axis: Vec3
+    pub z_axis: Vec3,
+    pub time_0: f64,
+    pub time_1: f64
 }
 
 impl Camera {
-    pub fn new(look_from: Point3, look_at: Point3, up: Vec3, vertical_fov: f64, aspect_ratio: f64, aperture: f64, focus_dist: f64) -> Self {
+    pub fn new(look_from: Point3, look_at: Point3, up: Vec3, vertical_fov: f64, aspect_ratio: f64, aperture: f64, focus_dist: f64, time_0: f64, time_1: f64) -> Self {
         let z_axis = (look_from - look_at).normalized();
         let x_axis = Vec3::cross(&up, &z_axis).normalized();
         let y_axis = Vec3::cross(&z_axis, &x_axis).normalized();
@@ -25,11 +29,17 @@ impl Camera {
             focus_dist: focus_dist,
             x_axis: x_axis,
             y_axis: y_axis,
-            z_axis: z_axis
+            z_axis: z_axis,
+            time_0: time_0,
+            time_1: time_1
         }
     }
 
     pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+        let mut rng = thread_rng();
+
+        let time = rng.gen_range(self.time_0..=self.time_1);
+
         let viewport_height = 2.0 * (self.vertical_fov / 2.0).tan();
         let viewport_width = viewport_height * self.aspect_ratio;
 
@@ -43,6 +53,6 @@ impl Camera {
         let ray_origin = self.position + sample_lens.x * self.x_axis + sample_lens.y * self.y_axis;
         let ray_direction = lower_left_corner + (u * horizontal) + (v * vertical) - ray_origin;
 
-        Ray::new(ray_origin, ray_direction)
+        Ray::with_time(ray_origin, ray_direction, time)
     }
 }
