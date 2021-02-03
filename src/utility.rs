6a7cf2d8399571922a4c9b05cd25a55753f46916
Luca <lams3@cin.ufpe.br>
2021-02-03 10:44:25 -0300

    InverseLerp for f64

--- src/utility.rs
+++ src/utility.rs
@@ -1,7 +1,7 @@
-pub trait Remapable {
+pub trait Remapable : Sized {
     type Output;
 
-    fn remap(&self, from: (Self::Output, Self::Output), to: (Self::Output, Self::Output)) -> Self::Output;
+    fn remap(&self, from: (Self, Self), to: (Self::Output, Self::Output)) -> Self::Output;
 }
 
 impl Remapable for f64 {
@@ -9,5 +9,15 @@ impl Remapable for f64 {
     
     fn remap(&self, from: (f64, f64), to: (f64, f64)) -> Self::Output {
         to.0 + (self - from.0) * (to.1 - to.0) / (from.1 - from.0)
+    }
+}
+
+pub trait InverseLerp {
+    fn inverse_lerp(a: Self, b: Self, v: Self) -> f64;
+}
+
+impl InverseLerp for f64 {
+    fn inverse_lerp(a: Self, b: Self, v: Self) -> f64 {
+        (v - a) / (b - a)
     }
 }
