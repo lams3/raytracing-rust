ed8d4364d3513363d37853b48623cc2664b14208
Luca <lams3@cin.ufpe.br>
2021-02-03 13:07:08 -0300

    AABB utility

--- src/structures/aabb.rs
+++ src/structures/aabb.rs
@@ -13,6 +13,34 @@ impl AABB {
         }
     }
 
+    pub fn from_points(points: &Vec<Point3>) -> Self {
+        let mut min = points[0];
+        let mut max = points[0];
+
+        for p in points {
+            for i in 0..3 {
+                if p[i] < min[i] {
+                    min[i] = p[i]
+                }
+
+                if p[i] > max[i] {
+                    max[i] = p[i]
+                }
+            }
+        }
+
+        Self::new(min, max)
+    }
+
+    pub fn encapsulate(&mut self, other: AABB) {
+        let mut points_0 = self.get_points().iter().copied().collect();
+        let mut points_1 = other.get_points().iter().copied().collect();
+        let mut points = vec![];
+        points.append(&mut points_0);
+        points.append(&mut points_1);
+        *self = AABB::from_points(&points)
+    }
+
     pub fn hit(&self, ray: Ray, t_min: f64, t_max: f64) -> bool {
         let mut t_min = t_min;
         let mut t_max = t_max;
@@ -33,5 +61,18 @@ impl AABB {
         }
 
         true
+    }
+
+    pub fn get_points(&self) -> [Point3; 8] {
+        [
+            Point3::new(self.min.x, self.min.y, self.min.z),
+            Point3::new(self.min.x, self.min.y, self.max.z),
+            Point3::new(self.min.x, self.max.y, self.min.z),
+            Point3::new(self.min.x, self.max.y, self.max.z),
+            Point3::new(self.max.x, self.min.y, self.min.z),
+            Point3::new(self.max.x, self.min.y, self.max.z),
+            Point3::new(self.max.x, self.max.y, self.min.z),
+            Point3::new(self.max.x, self.max.y, self.max.z),
+        ]
     }
 }
