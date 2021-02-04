66b929ad579ff38bb557b30b875f8fc02461f67c
Luca <lams3@cin.ufpe.br>
2021-02-04 10:56:40 -0300

    MovingInstance refactor and bounding box correction

--- src/hittables/moving_instance.rs
+++ src/hittables/moving_instance.rs
@@ -22,13 +22,17 @@ impl MovingInstance {
             time_1: time_1
         }
     }
+
+    pub fn transform_at(&self, time: f64) -> Transform {
+        let t = f64::inverse_lerp(self.time_0, self.time_1, time);
+        Transform::interpolate(self.transform_0, self.transform_1, t)
+    }
 }
 
 impl Hittable for MovingInstance {
     
     fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
-        let t = f64::inverse_lerp(self.time_0, self.time_1, ray.time);
-        let transform = Transform::interpolate(self.transform_0, self.transform_1, t);
+        let transform = self.transform_at(ray.time);
         let ray = transform.inverse_transform_ray(*ray);
         self.hittable.hit(&ray, t_min, t_max)
     }
@@ -36,8 +40,10 @@ impl Hittable for MovingInstance {
     fn bounding_box(&self, time_0: f64, time_1: f64) -> Option<AABB> {
         match self.hittable.bounding_box(time_0, time_1) {
             Some(aabb) => {
-                let mut points_0 = aabb.get_points().iter().map(|&el| self.transform_0.transform_point(el)).collect();
-                let mut points_1 = aabb.get_points().iter().map(|&el| self.transform_1.transform_point(el)).collect();
+                let transform_0 = self.transform_at(time_0);
+                let transform_1 = self.transform_at(time_1);
+                let mut points_0 = aabb.get_points().iter().map(|&el| transform_0.transform_point(el)).collect();
+                let mut points_1 = aabb.get_points().iter().map(|&el| transform_1.transform_point(el)).collect();
                 let mut points = Vec::with_capacity(16);
                 points.append(&mut points_0);
                 points.append(&mut points_1);
