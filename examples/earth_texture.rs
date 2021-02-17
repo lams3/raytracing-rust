5b0607b75ce90502efe6cacdc62bc48601bae856
Luca <lams3@cin.ufpe.br>
2021-02-17 12:41:26 -0300

    ImageTexture + Demo

---
+++ examples/earth_texture.rs
@@ -0,0 +1,59 @@
+extern crate raytracer;
+
+use raytracer::rendering::render;
+use raytracer::rendering::skyboxes::GradientSkybox;
+use raytracer::rendering::Camera;
+use raytracer::rendering::RenderParams;
+use raytracer::textures::ImageTexture;
+use raytracer::structures::{Color, Vec3, Point3};
+use raytracer::hittables::{BVHNode, HittableList, Sphere};
+use raytracer::materials::Lambertian;
+
+use std::sync::Arc;
+use std::time::Instant;
+
+use pbr::ProgressBar;
+
+const WIDTH: usize = 1280;
+const HEIGHT: usize = 720;
+const NUM_SAMPLES: u32 = 100;
+const MAX_RAY_DEPTH: u32 = 50;
+
+fn main() {
+    let aspect_ratio = WIDTH as f64 / HEIGHT as f64;
+    let mut progress_bar = ProgressBar::new(NUM_SAMPLES as u64);
+    
+    let params = RenderParams {
+        image_width: WIDTH,
+        image_height: HEIGHT,
+        num_samples: NUM_SAMPLES,
+        max_ray_depth: MAX_RAY_DEPTH
+    };
+    let camera = Arc::new(Camera::new(Point3::new(13.0, 2.0, 3.0), Point3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 1.0, 0.0), (20.0 as f64).to_radians(), aspect_ratio, 0.0, 10.0, 0.0, 1.0));
+    let skybox = Arc::new(GradientSkybox::new(Color::new(1.0, 1.0, 1.0), Color::new(0.5, 0.7, 1.0), Vec3::new(0.0, 1.0, 0.0)));
+    let world = Arc::new(BVHNode::new(&build_scene(), 0.0, 1.0));
+    
+    progress_bar.set(0);
+
+    let start = Instant::now();
+
+    render(world, skybox, camera, &params, move |sampled, _| {
+        progress_bar.set(sampled as u64);
+    }).save("./earth_texture.png");
+
+    let duration = start.elapsed();
+
+    println!("Time Elapsed: {:?}", duration);
+}
+
+fn build_scene() -> HittableList {
+    let mut world = HittableList::new();
+
+    let earth_texture = Arc::new(ImageTexture::read("./resources/earthmap.jpg"));
+    let material = Arc::new(Lambertian::new(earth_texture));
+    
+    let earth = Arc::new(Sphere::new(Point3::new(0.0, 0.0, 0.0), 2.0, material.clone()));
+    world.add(earth);
+
+    world
+}
